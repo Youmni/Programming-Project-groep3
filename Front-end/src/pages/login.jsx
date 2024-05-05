9381117071d8acbdcf4374eaca29d445d1de7d0b
clauwaesenne <clauwaesenne@gmail.com>
2024-05-05 23:31:45 +0200

    Front end branche, Back-end verwijderd

--- Front-end/src/pages/login.jsx
+++ Front-end/src/pages/login.jsx
@@ -1,28 +1,60 @@
 import React from "react";
 import { IoInformationCircleSharp } from "react-icons/io5";
-import { Link, Navigate } from "react-router-dom";
-
+import { Link, useNavigate } from "react-router-dom";
 
 const login = () => {
+  const navigate = useNavigate();
+
+  const handleClick = () => {
+      navigate("/home");
+  };
+
   return (
     <div className="flex w-full h-screen bg-slate-100 justify-center items-center">
       <form action="" className="flex w-3/5 h-3/4 shadow-lg rounded-md ">
         <div className="flex w-1/2 h-full bg-white pl-10 pt-10 pr-10 flex-col items-center">
           <header className="flex w-full h-20  items-center justify-between">
-           <h1 className="text-4xl font-light">Log in</h1>
-           <Link to={`https://www.erasmushogeschool.be/nl/onderzoek/labs/medialab.brussels/medialab`} target="blank">
-           <IoInformationCircleSharp className="size-7"/>
-           </Link>
+            <h1 className="text-4xl font-light">Log in</h1>
+            <Link
+              to={`https://www.erasmushogeschool.be/nl/onderzoek/labs/medialab.brussels/medialab`}
+              target="blank"
+            >
+              <IoInformationCircleSharp className="size-7" />
+            </Link>
           </header>
           <div className="flex flex-col w-full gap-2 mt-10">
-            <label htmlFor="" className="font-bold">Gebruikersnaam</label>
-            <input type="text" name="" id="" placeholder="Gebruikersnaam" className="h-[50px] pl-4 rounded-2xl bg-gray-100"/>
-            <label htmlFor="" className="font-bold mt-10">Wachtwoord</label>
-            <input type="password" name="" id="" placeholder="Wachtwoord" className="h-[50px] pl-4 rounded-2xl bg-gray-100"/>
+            <label htmlFor="" className="font-bold">
+              Gebruikersnaam
+            </label>
+            <input
+              type="text"
+              name=""
+              id=""
+              placeholder="Gebruikersnaam"
+              className="h-[50px] pl-4 rounded-2xl bg-gray-100"
+            />
+            <label htmlFor="" className="font-bold mt-10">
+              Wachtwoord
+            </label>
+            <input
+              type="password"
+              name=""
+              id=""
+              placeholder="Wachtwoord"
+              className="h-[50px] pl-4 rounded-2xl bg-gray-100"
+            />
           </div>
           <div className="flex flex-col w-full gap-5 mt-20">
-            <button onClick={() => <Link to={`/http://localhost:5173/home`}></Link> } className="w-full h-12 bg-red-400 rounded-3xl text-white hover:bg-red-700">Log in</button>
-            <Link to="/register" className="text-red-500 text-center">Nog geen account? Registreer <span className="underline">hier</span></Link>
+            <button
+              onClick={handleClick}
+              className="w-full h-12 bg-red-400 rounded-3xl text-white hover:bg-red-700"
+            >
+              Log in
+            </button>
+            <Link to="/register" className="text-red-500 text-center">
+              Nog geen account? Registreer{" "}
+              <span className="underline">hier</span>
+            </Link>
           </div>
         </div>
         <div className="flex w-1/2 bg-gradient-to-r from-red-300 to-red-500">
@@ -32,7 +64,8 @@ const login = () => {
                 Welkom,
               </span>
               <span className="text-4xl text-center text-white font-semibold">
-                Bij het medialab <span className="font-bold text-6xl">uitleendienst</span>
+                Bij het medialab{" "}
+                <span className="font-bold text-6xl">uitleendienst</span>
               </span>
             </h1>
           </header>
