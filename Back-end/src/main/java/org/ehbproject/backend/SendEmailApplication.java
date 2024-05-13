735a89e335a943d8c15bfa1afd4321d023a44d72
Thomas <thomas.van.der.borght@student.ehb.be>
2024-05-13 14:36:13 +0200

    email service gemaakt

---
+++ Back-end/src/main/java/org/ehbproject/backend/SendEmailApplication.java
@@ -0,0 +1,29 @@
+package org.ehbproject.backend;
+
+import org.springframework.context.ApplicationContext;
+import org.ehbproject.backend.services.emailservice.EmailService;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Profile;
+
+
+@SpringBootApplication
+public class SendEmailApplication {
+
+    private EmailService emailService;
+    public SendEmailApplication (EmailService emailService){
+        this.emailService = emailService;
+    }
+
+    public static void main(String[] args){
+        ApplicationContext context = SpringApplication.run(SendEmailApplication.class, args);
+        SendEmailApplication app = context.getBean(SendEmailApplication.class);
+
+        app.Run();
+    }
+
+    private void Run(){
+        emailService.SendMail("thomas.van.der.borght@student.ehb.be", "herinnering", "breng terug");
+    }
+}
